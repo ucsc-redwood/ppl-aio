09714e14141952e0ecb37bb541f485a3602dc2a1
Yanwen Xu <xuyanwen2012@gmail.com>
2024-10-19 21:58:11 -0700

    re organize

--- tests/foundations/xmake.lua
+++ tests/foundations/xmake.lua
@@ -2,12 +2,18 @@ target("test-glm")
     set_kind("binary")
     add_files("test-glm.cpp")
     add_packages("gtest", "glm")
-target_end()
 
 target("test-vulkan")
     set_kind("binary")
     add_includedirs("$(projectdir)/include")
     add_files("test-vulkan.cpp")
     add_packages("gtest", "vulkan-headers")
-    add_packages("vulkansdk")
+
+if is_plat("linux") then
+target("test-cuda")
+    set_kind("binary")
+    add_files("test-cuda.cu")
+    add_packages("gtest")
+    add_cugencodes("native")
+end
 
